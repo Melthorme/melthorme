504a0ce63e5a98e4aa7dbba9d6d0b47f383719ee
Melthorme <87016380+Melthorme@users.noreply.github.com>
2021-09-07 15:32:29 +0200

    Add cross browser support

--- try.tests/src/main/java/com/automationpractice/base/BaseTest.java
+++ try.tests/src/main/java/com/automationpractice/base/BaseTest.java
@@ -2,20 +2,45 @@ package com.automationpractice.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
     protected WebDriver driver;
     protected WebElement element;
 
-
+    @Parameters({ "browser"})
     @BeforeMethod
-    public void setup() {
-        System.setProperty("webdriver.edge.driver", "E:/itprojects/com.automationpractice/src/main/resources/msedgedriver.exe");
-        driver = new EdgeDriver();
+    public void setUp(@Optional("edge") String browser) {
+        switch(browser) {
+            case "edge":
+                System.setProperty("webdriver.edge.driver", "E:/itprojects/com.automationpractice/src/main/resources/msedgedriver.exe");
+                driver = new EdgeDriver();
+                break;
+
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "E:/itprojects/com.automationpractice/src/main/resources/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "E:/itprojects/com.automationpractice/src/main/resources/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                System.out.println("Do not know how to start " + browser + ", starting Edge instead.");
+                System.setProperty("webdriver.edge.driver", "E:/itprojects/com.automationpractice/src/main/resources/msedgedriver.exe");
+                driver = new EdgeDriver();
+                break;
+        }
+
         driver.manage().window().maximize();
     }
 
