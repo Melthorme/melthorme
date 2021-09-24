f4a466202ffee175ca24afb4d217cea22f1e830a
Melthorme <87016380+Melthorme@users.noreply.github.com>
2021-09-24 16:19:02 +0200

    Fix imports on BaseTest

--- com.automationpractice/src/main/java/com/automationpractice/base/BaseTest.java
+++ com.automationpractice/src/main/java/com/automationpractice/base/BaseTest.java
@@ -3,7 +3,6 @@ package com.automationpractice.base;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,10 +12,9 @@ import org.testng.annotations.Parameters;
 public class BaseTest {
 
     protected WebDriver driver;
-    protected WebElement element;
     protected Logger log;
 
-    @Parameters({ "browser"})
+    @Parameters({"browser"})
     @BeforeMethod
     public void setUp(@Optional("edge") String browser, ITestContext ctx) {
         String testName = ctx.getCurrentXmlTest().getName();
