467b9a2674e5bd8bbb775bcf4904b649d8c7dc63
Melthorme <87016380+Melthorme@users.noreply.github.com>
2021-09-27 11:33:07 +0200

    Delete job.seek.automation directory

--- job.seek.automation/JobSeekSuite.java
+++
@@ -1,9 +0,0 @@
-package automation.seek.job;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({Bulldog.class, NoFluff.class, JustJoin.class, Pracuj.class})
-public class JobSeekSuite {
-}
--- job.seek.automation/readme.txt
+++
@@ -1,8 +0,0 @@
-Hello there!
-
-This is my job seek automation with Selenium Java (16) and JUnit for Edge browser.
-
-If You have any sugestions please write at "kingmosse @ gmail . com".
-
-Cheers!
-Greg
